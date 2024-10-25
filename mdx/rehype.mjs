1ffb4862b551d818a1b946ac28ccb6376b37f957
Arsen Shkrumelyak <arsen@shkrumelyak.com>
2024-10-25 12:15:52 +0100

    Peace.

--- mdx/rehype.mjs
+++ mdx/rehype.mjs
@@ -6,30 +6,7 @@ const rehypeStagger = () => {
 	return (tree) => {
 		staggerIndex = 0;
 		visit(tree, "element", (node) => {
-			console.log(node);
-			/*
-			{
-				type: 'element',
-				tagName: 'h1',
-				properties: {},
-				children: [
-					{
-						type: 'mdxTextExpression',
-						value: 'writing.title',
-						position: [Object],
-						data: [Object]
-					}
-				],
-				position: {
-					start: { line: 10, column: 1, offset: 122 },
-					end: { line: 10, column: 18, offset: 139 }
-				}
-			}
-			*/
-
 			node.properties.style = `${node.properties.style || ""} --stagger-index: ${staggerIndex};`;
-			console.log(node);
-
 			staggerIndex++;
 		});
 	};
