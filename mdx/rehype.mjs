01e6f9519417a9a0461cc8ffca47dd74478c7955
Arsen Shkrumelyak <arsen@shkrumelyak.com>
2024-10-29 12:17:21 +0000

    Language.

--- mdx/rehype.mjs
+++ mdx/rehype.mjs
@@ -6,6 +6,13 @@ const rehypeStagger = () => {
 	return (tree) => {
 		staggerIndex = 0;
 		visit(tree, "element", (node) => {
+			if (node.tagName === "a") {
+				return;
+			}
+			if (node.tagName === "blockquote") {
+				node.properties.className = "orchestration";
+				return;
+			}
 			node.properties.style = `${node.properties.style || ""} --stagger-index: ${staggerIndex};`;
 			staggerIndex++;
 		});
