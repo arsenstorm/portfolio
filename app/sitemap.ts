bf02d8de1557ab1df17eb024be7a5576a27b41b4
Arsen Shkrumelyak <arsen@shkrumelyak.com>
2024-11-01 09:26:04 +0000

    sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,38 @@
+// Types
+import type { MetadataRoute } from "next";
+
+// Utils
+import { getAllWriting } from "@/utils/get-all-writing";
+
+export async function generateSitemaps() {
+	return [{ id: 0 }];
+}
+
+export default async function sitemap({
+	id,
+}: {
+	id: number;
+}): Promise<MetadataRoute.Sitemap> {
+	const start = id * 50000;
+	const end = start + 50000;
+	const writings = await getAllWriting();
+	const writingSitemap = writings.slice(start, end).map((writing) => ({
+		url: `https://arsenstorm.com/writing/${writing.slug}`,
+		lastModified: writing.date,
+	}));
+
+	const pages = {
+		home: {
+			url: "https://arsenstorm.com",
+			lastModified: new Date().toISOString(),
+		},
+		writing: {
+			url: "https://arsenstorm.com/writing",
+			lastModified: new Date().toISOString(),
+		},
+	};
+
+	const sitemap = [...Object.values(pages), ...writingSitemap];
+
+	return sitemap;
+}
