b32365e2eb63887571971b53078989b96c1db41a
Arsen Shkrumelyak <arsen@shkrumelyak.com>
2024-12-07 15:55:58 +0000

    force dynamic globe page

--- app/globe/page.tsx
+++ app/globe/page.tsx
@@ -4,6 +4,8 @@ import { getVisitors } from "@/actions/get-visitors";
 // Components
 import Globe from "./page.client";
 
+export const dynamic = "force-dynamic";
+
 export default async function GlobePage() {
 	const { visitors, lastUpdated } = await getVisitors();
 
