5336dc8566b815712dbd6fdce2c2ab2cc1082691
Arsen Shkrumelyak <arsen@shkrumelyak.com>
2024-12-07 16:09:49 +0000

    use revalidate instead of dynamic

--- actions/get-visitors.ts
+++ actions/get-visitors.ts
@@ -1,12 +1,15 @@
 import type { VisitorLog } from "@/app/api/track/route";
 import { kv } from "@vercel/kv";
 
-export const dynamic = "force-dynamic";
+export const revalidate = 3600;
 
 export async function getVisitors(): Promise<{
 	visitors: VisitorLog[];
 	lastUpdated: number;
 }> {
 	const visitors = (await kv.get("visitors")) as VisitorLog[];
-	return { visitors: visitors ?? [], lastUpdated: Date.now() };
+	return {
+		visitors: visitors ?? [],
+		lastUpdated: Date.now() - 10000,
+	};
 }
