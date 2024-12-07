9dc95e022667f3d3718778d585bc031d7d40cd18
Arsen Shkrumelyak <arsen@shkrumelyak.com>
2024-12-07 13:40:47 +0000

    force dynamic

--- actions/get-visitors.ts
+++ actions/get-visitors.ts
@@ -1,6 +1,8 @@
 import type { VisitorLog } from "@/app/api/track/route";
 import { kv } from "@vercel/kv";
 
+export const dynamic = "force-dynamic";
+
 export async function getVisitors(): Promise<VisitorLog[]> {
 	const visitors = (await kv.get("visitors")) as VisitorLog[];
 	return visitors ?? [];
