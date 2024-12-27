6ada363b9c708c7c1ef38a867f0967d7e8752a79
Arsen Shkrumelyak <arsen@shkrumelyak.com>
2024-12-27 22:12:00 +0000

    Judge. This patch also adds a new page for the meaning of each writing which I'll be working on.

--- app/writing/[slug]/meaning/page.tsx
+++ app/writing/[slug]/meaning/page.tsx
@@ -1,5 +1,56 @@
+// Next
 import { notFound } from "next/navigation";
 
-export default function Page() {
-	return notFound();
+// Design
+import { EscapeTitle } from "@/components/design/escape";
+import WritingPageClient from "../page.client";
+
+// Utils
+import { readWriting } from "@/mdx/compile";
+import { getAllWriting } from "@/utils/get-all-writing";
+
+export default async function MeaningPage({
+	params,
+}: Readonly<{
+	params: Promise<{ slug: string }>;
+}>) {
+	const { slug } = await params;
+	const [writings] = await Promise.all([getAllWriting()]);
+
+	const index = writings.findIndex((w) => w.slug === slug);
+
+	const writing = writings[index];
+
+	if (!writing) {
+		return notFound();
+	}
+
+	const markdown =
+		((await readWriting(writing.slug)) ?? "")
+			.replace(/---[\s\S]*?---\s*/, "")
+			.split(/---\s*/)[1] ??
+		`I haven’t written about the meaning of **${writing?.title ?? "Untitled."}** yet.`;
+
+	return (
+		<main className="prose dark:prose-invert">
+			<EscapeTitle
+				title={`The meaning of ${writing?.title ?? "Untitled."}`}
+				//audioUrl={`/writing/${slug}/meaning/audio`} // TODO: Add audio for the meaning
+				extraLink={`/writing/${slug}`}
+				extraLinkHint={{
+					action: "Go back to",
+					this: writing?.title ?? "Untitled.",
+				}}
+			/>
+			<WritingPageClient markdown={markdown} />
+		</main>
+	);
+}
+
+export async function generateStaticParams() {
+	const writings = await getAllWriting();
+
+	return writings.map((writing) => ({
+		slug: writing.slug,
+	}));
 }
--- app/writing/[slug]/page.tsx
+++ app/writing/[slug]/page.tsx
@@ -1,5 +1,4 @@
 // UI
-import clsx from "clsx";
 import { Button } from "@/components/ui/button";
 
 // Utils
@@ -92,44 +91,14 @@ export default async function WritingPage({
 	const next = writings?.[index - 1]?.slug;
 	const writing = writings[index];
 
-	const markdown = ((await readWriting(writing.slug)) ?? "").replace(
-		/---[\s\S]*?---\s*/,
-		"",
-	);
+	const markdown =
+		((await readWriting(writing.slug)) ?? "")
+			.replace(/---[\s\S]*?---\s*/, "")
+			.split(/---\s*/)[0] ?? "";
 
 	return (
 		<>
-			<main
-				className={clsx(
-					// Animations
-					"orchestration",
-					// Remove gap
-					"-mt-4",
-					// Base
-					"prose dark:prose-invert",
-					// Headings
-					"prose-h1:text-2xl/8 prose-h1:font-semibold prose-h1:text-zinc-950 prose-h1:sm:text-xl/8 prose-h1:dark:text-white",
-					// Subheadings
-					"prose-h2:text-base/7 prose-h2:font-semibold prose-h2:text-zinc-950 prose-h2:sm:text-sm/6 prose-h2:dark:text-white",
-					// Text
-					"prose-p:text-base/6 prose-p:text-zinc-500 prose-p:sm:text-sm/6 prose-p:dark:text-zinc-400",
-					// Links
-					"prose-a:text-zinc-950 prose-a:underline prose-a:underline-offset-2 prose-a:decoration-zinc-950/50",
-					"prose-a:data-[hover]:decoration-zinc-950 prose-a:dark:text-white prose-a:dark:decoration-white/50 prose-a:dark:data-[hover]:decoration-white",
-					// Strong
-					"prose-strong:font-medium prose-strong:text-zinc-950 prose-strong:dark:text-white",
-					// Code
-					"prose-code:rounded prose-code:border prose-code:border-zinc-950/10 prose-code:bg-zinc-950/[2.5%]",
-					"prose-code:px-0.5 prose-code:text-sm prose-code:font-medium prose-code:text-zinc-950 prose-code:sm:text-[0.8125rem]",
-					"prose-code:dark:border-white/20 prose-code:dark:bg-white/5 prose-code:dark:text-white",
-					// Blockquotes
-					"prose-blockquote:border-l-0 prose-blockquote:pl-0",
-					"prose-blockquote:italic prose-blockquote:font-medium",
-					"prose-blockquote:text-zinc-950 dark:prose-blockquote:text-zinc-50",
-					// Dividers
-					"prose-hr:my-8 prose-hr:border-zinc-950/10 prose-hr:dark:border-white/10",
-				)}
-			>
+			<main>
 				<EscapeTitle
 					title={writing?.title ?? "Untitled."}
 					audioUrl={`/writing/${slug}/audio`}
